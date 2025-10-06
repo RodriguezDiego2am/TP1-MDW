650c048c1e3d31c15b909bef521f9627e1891b45
Diego Joaquin Rodriguez <DiegoJoaquin.Rodriguez@alumnos.uai.edu.ar>
2025-10-06 14:01:38 -0300

    Repositorio clonado

---
+++ src/index.ts
@@ -0,0 +1,41 @@
+import express, { NextFunction, Request, Response } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+import userRoutes from "./routes/userRoutes";
+import loginRoutes from "./routes/loginRoutes";
+import { authMiddleware } from "./middlewares/authMiddleware";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT;
+const mongoUri = process.env.MONGO_URI!;
+
+app.use(cors({
+  origin: ['http://127.0.0.1:5500', 'http://localhost:5500'],
+  credentials: true,
+}));
+app.use(express.json({ limit: "10mb" }));
+app.use(cookieParser());
+
+app.use("/api/users", authMiddleware, userRoutes);
+app.use("/api/auth", loginRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+const connectToDb = async () => {
+  try {
+    await mongoose.connect(mongoUri, {});
+    console.log("MongoDB conectado");
+  } catch (error) {
+    console.error(`Error de conexión a MongoDB: ${error}`);
+  }
+};
+connectToDb();
